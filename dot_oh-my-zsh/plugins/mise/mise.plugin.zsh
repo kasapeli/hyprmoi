b4a6c6fbd06a2ee1857c949fb497d1e60dbed080
kasapeli <yanratanaksambath@gmail.com>
2026-08-26 17:34:29 +0700

    add oh my zsh files and missing packages

---
+++ dot_oh-my-zsh/plugins/mise/mise.plugin.zsh
@@ -0,0 +1,21 @@
+if (( ! $+commands[mise] )); then
+  return
+fi
+
+# Load mise hooks
+eval "$(mise activate zsh)"
+
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `mise`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_mise" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _mise
+  _comps[mise]=_mise
+fi
+
+# Generate and load mise completion
+zmodload -F zsh/files b:zf_mv
+() {
+  local TMPPREFIX="$ZSH_CACHE_DIR/completions/_mise"
+  zf_mv -f -- =( mise completion zsh ) "$TMPPREFIX"
+} &|
